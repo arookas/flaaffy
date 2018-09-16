7b903d1f67b2d77bf7aa62282ab8e4b6ee933484
arookas <arookas@outlook.com>
2018-09-16 13:53:20 -0400

    Change project framework to 4.6.1

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,38 @@
 
 workspace "flaaffy"
-	configurations { "Debug", "Release" }
-	targetdir "bin/%{cfg.buildcfg}"
-	startproject "mareep"
-	
-	filter "configurations:Debug"
-		defines { "DEBUG" }
-		flags { "Symbols" }
-	
-	filter "configurations:Release"
-		defines { "RELEASE" }
-		optimize "On"
-	
-	project "mareep"
-		kind "ConsoleApp"
-		language "C#"
-		namespace "arookas"
-		location "mareep"
-		entrypoint "arookas.mareep"
-		targetname "mareep"
-		
-		links { "arookas", "System", "System.Xml", "System.Xml.Linq", }
-		
-		files {
-			"mareep/**.cs",
-		}
-		
-		excludes {
-			"mareep/bin/**",
-			"mareep/obj/**",
-		}
-	
+configurations { "Debug", "Release" }
+targetdir "bin/%{cfg.buildcfg}"
+startproject "mareep"
+
+filter "configurations:Debug"
+defines { "DEBUG" }
+symbols "on"
+
+filter "configurations:Release"
+defines { "RELEASE" }
+optimize "On"
+
+project "mareep"
+kind "ConsoleApp"
+language "C#"
+namespace "arookas"
+location "mareep"
+entrypoint "arookas.mareep"
+targetname "mareep"
+framework "4.6.1"
+
+links {
+	"arookas",
+	"System",
+	"System.Xml",
+	"System.Xml.Linq",
+}
+
+files {
+	"mareep/**.cs",
+}
+
+excludes {
+	"mareep/bin/**",
+	"mareep/obj/**",
+}
